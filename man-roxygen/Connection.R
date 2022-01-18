cfeb4cd385d3e731cc81b9b230002ab753253d6c
mdlavallee92 <lavalleema@mymail.vcu.edu>
2022-01-18 15:27:01 -0500

    fix description file and add connectionDetails roxygen

--- man-roxygen/Connection.R
+++ man-roxygen/Connection.R
@@ -1,7 +1,9 @@
-#' @param connection   Either an object of type \code{connectionDetails} as created using the
-#'                     \code{\link[DatabaseConnector]{createConnectionDetails}} function in the
-#'                     DatabaseConnector package or or object of type \code{connection} as created using the
-#'                     \code{\link[DatabaseConnector]{connect}} function in the
-#'                     DatabaseConnector package. If  \code{connectionDetails}
-#'                     is provided a new connection will be opened at the start
-#'                     of the function and closed when the function finishes.
+#' @param connectionDetails   An object of type \code{connectionDetails} as created using the
+#'                            \code{\link[DatabaseConnector]{createConnectionDetails}} function in the
+#'                            DatabaseConnector package. Can be left NULL if \code{connection} is
+#'                            provided.
+#' @param connection          An object of type \code{connection} as created using the
+#'                            \code{\link[DatabaseConnector]{connect}} function in the
+#'                            DatabaseConnector package. Can be left NULL if \code{connectionDetails}
+#'                            is provided, in which case a new connection will be opened at the start
+#'                            of the function, and closed when the function finishes.
