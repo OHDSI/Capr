122675addc1792b4c2369ecad15d693d6c298cca
Adam Black <ablack3@users.noreply.github.com>
2022-11-13 12:23:26 +0000

    Temporarily use old concept class defintions. Move query class definition to query.R

--- R/classes.R
+++ R/classes.R
@@ -20,39 +20,39 @@ setClass("Occurrence",
 )
 
 # Concept Classes ---------------------------------
-
-setClass("Concept",
-         slot = c(
-           conceptId = "integer",
-           conceptName = "character",
-           standardConcept = "character",
-           standardConceptCaption = "character",
-           invalidReason = "character",
-           invalidReasonCaption = "character",
-           conceptCode = "character",
-           domainId = "character",
-           vocabularyId = "character",
-           conceptClassId = "character",
-           includeDescendants = "logical",
-           includeMapped = "logical",
-           isExcluded = "logical"
-         ),
-         prototype = list(
-           conceptId = NA_integer_,
-           conceptName = NA_character_,
-           standardConcept = NA_character_,
-           standardConceptCaption = NA_character_,
-           invalidReason = NA_character_,
-           invalidReasonCaption = NA_character_,
-           conceptCode = NA_character_,
-           domainId = NA_character_,
-           vocabularyId = NA_character_,
-           conceptClassId = NA_character_,
-           includeDescendants = FALSE,
-           includeMapped = FALSE,
-           isExcluded = FALSE
-         )
-)
+#
+# setClass("Concept",
+#          slot = c(
+#            conceptId = "integer",
+#            conceptName = "character",
+#            standardConcept = "character",
+#            standardConceptCaption = "character",
+#            invalidReason = "character",
+#            invalidReasonCaption = "character",
+#            conceptCode = "character",
+#            domainId = "character",
+#            vocabularyId = "character",
+#            conceptClassId = "character",
+#            includeDescendants = "logical",
+#            includeMapped = "logical",
+#            isExcluded = "logical"
+#          ),
+#          prototype = list(
+#            conceptId = NA_integer_,
+#            conceptName = NA_character_,
+#            standardConcept = NA_character_,
+#            standardConceptCaption = NA_character_,
+#            invalidReason = NA_character_,
+#            invalidReasonCaption = NA_character_,
+#            conceptCode = NA_character_,
+#            domainId = NA_character_,
+#            vocabularyId = NA_character_,
+#            conceptClassId = NA_character_,
+#            includeDescendants = FALSE,
+#            includeMapped = FALSE,
+#            isExcluded = FALSE
+#          )
+# )
 
 
 
@@ -70,18 +70,7 @@ setClass("ConceptSet",
 )
 
 # Circe Classes --------------------------------------
-setClass("Query",
-         slot = c(
-           domain = "character",
-           conceptSet = "ConceptSet",
-           attributes = "list"
-         ),
-         prototype = list(
-           domain = NA_character_,
-           conceptSet = new("ConceptSet"),
-           attributes = list()
-         )
-)
+
 
 # setClass("Count",
 #          slots = c(
--- R/query.R
+++ R/query.R
@@ -1,7 +1,50 @@
 
+#' An S4 class for a Circe Query
+#'
+#' A query is a medical concept that can be extracted from a database through a 'where' clause in a SQL statement.
+#' This includes concepts.
+#'
+#' @slot domain The domain to search (e.g. "Condition", "Drug", "Measurement", etc)
+#' @slot conceptSet The Concept set describing the observation to serach for
+#' @slot attributes a list of attributes that modify the query (e.g. `male()`, `female()`, `age(gte(65))`)
+setClass("Query",
+         slot = c(
+           domain = "character",
+           conceptSet = "ConceptSet",
+           attributes = "list"
+         ),
+         prototype = list(
+           domain = NA_character_,
+           conceptSet = new("ConceptSet"),
+           attributes = list()
+         )
+)
+
+setValidity("Query", function(object) {
+  validDomains <- c("Condition", "conditionOccurrence", "Observation", "Drug", "Measurement") #...
+  stopifnot(object@domain %in% validDomains)
+  TRUE
+})
+
+#' @rdname show-method
+#' @aliases show,Query-method
+setMethod("show", "Query", function(object) {
+  cat(glue::glue("<Capr {object@domain} Query> {object@conceptSet@Name}"), "\n")
+  # cat("Attributes:", "\n")
+  # if (length(object@Attributes) > 0) {
+  #   for (i in seq_along(object@Attributes)) {
+  #     cat("\t",paste0(i, ") "))
+  #     show(object@Attributes[[i]])
+  #     cat("\n")
+  #   }
+  # } else {
+  #   cat("None", "\n")
+  # }
+})
+
 query <- function(domain, conceptSet, ...) {
 
-  #bundle attributes as a list
+  # bundle attributes as a list
   atb <- list(...)
 
   query <- new("Query",
@@ -12,6 +55,13 @@ query <- function(domain, conceptSet, ...) {
 
 }
 
+#' Query the condition domain
+#'
+#' @param conceptSet A condition concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
 conditionOccurrence <- function(conceptSet, ...) {
 
   query(domain = "conditionOccurrence",
