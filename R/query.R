02bc4b1880e99e576ca29a6d8caca18cc9ec8dd7
Adam Black <ablack3@users.noreply.github.com>
2022-11-14 01:01:27 +0000

    commit work on query.R

--- R/query.R
+++ R/query.R
@@ -1,4 +1,6 @@
 
+# Class definition -----
+
 #' An S4 class for a Circe Query
 #'
 #' A query is a medical concept that can be extracted from a database through a 'where' clause in a SQL statement.
@@ -21,15 +23,54 @@ setClass("Query",
 )
 
 setValidity("Query", function(object) {
-  validDomains <- c("Condition", "conditionOccurrence", "Observation", "Drug", "Measurement") #...
+  validDomains <- c("ConditionEra",
+                    "ConditionOccurrence",
+                    "Death",
+                    "DeviceExposure",
+                    "DoseEra",
+                    "DrugExposure",
+                    "Measurement",
+                    "Observation",
+                    "PayerPlanPeriod",
+                    "Specimen",
+                    "VisitOccurrence",
+                    "ObservationWindow")
   stopifnot(object@domain %in% validDomains)
+
+  domainsInConceptSet <- purrr::map_chr(object@conceptSet@Expression, ~.@Concept@domain_id) %>%
+    {.[!is.na(.)]} %>%
+    {.[. != ""]}
+
+  domainMap <- c("ConditionEra" = "Condition",
+                 "ConditionOccurrence" = "Condition",
+                 "DeviceExposure" = "Device",
+                 "DrugExposure" = "Drug",
+                 "Measurement" = "Measurement",
+                 "Specimen" = "Specimen",
+                 "VisitOccurrence" = "Visit")
+
+  # Print a warning if the concept set does not include concepts with the expected domain_id (domain_id must be populated)
+  if ((object@domain %in% names(domainMap)) &&
+      (length(domainsInConceptSet) > 0) &&
+      !(domainMap[object@domain] %in% domainsInConceptSet)) {
+    rlang::warn(glue::glue("{object@domain} query does contain concepts in {domainMap[object@domain]} domain."))
+  }
+
+  # TODO validation of attributes. Allowed attributes are query specific.
+
   TRUE
 })
+
+# Printing ----
 
 #' @rdname show-method
 #' @aliases show,Query-method
 setMethod("show", "Query", function(object) {
   cat(glue::glue("<Capr {object@domain} Query> {object@conceptSet@Name}"), "\n")
+
+  # TODO make this a one line print method
+  # cli::console_width() can give the available space in the console
+
   # cat("Attributes:", "\n")
   # if (length(object@Attributes) > 0) {
   #   for (i in seq_along(object@Attributes)) {
@@ -42,6 +83,9 @@ setMethod("show", "Query", function(object) {
   # }
 })
 
+
+# Constructors -----
+
 query <- function(domain, conceptSet, ...) {
 
   # bundle attributes as a list
@@ -52,7 +96,6 @@ query <- function(domain, conceptSet, ...) {
                conceptSet = conceptSet,
                attributes = atb)
   return(query)
-
 }
 
 #' Query the condition domain
@@ -67,30 +110,91 @@ condition <- function(conceptSet, ...) {
   query(domain = "ConditionOccurrence",
         conceptSet = conceptSet,
         ...)
-
 }
 
+#' Query the drug domain
+#'
+#' @param conceptSet A drug concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
 drug <- function(conceptSet, ...) {
 
-  query(domain = "drugExposure",
+  query(domain = "DrugExposure",
         conceptSet = conceptSet,
         ...)
-
 }
 
+#' Query the measurement domain
+#'
+#' @param conceptSet A measurement concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
 measurement <- function(conceptSet, ...) {
 
-  query(domain = "measurement",
+  query(domain = "Measurement",
         conceptSet = conceptSet,
         ...)
-
 }
 
+#' Query the procedure domain
+#'
+#' @param conceptSet A procedure concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
 procedure <- function(conceptSet, ...) {
 
   query(domain = "ProcedureOccurrence",
         conceptSet = conceptSet,
         ...)
-
 }
+
+#' Query the drug era domain
+#'
+#' @param conceptSet A drug ingredient concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
+drugEra <- function(conceptSet, ...) {
+
+  query(domain = "DrugEra",
+        conceptSet = conceptSet,
+        ...)
+}
+
+#' Query the condition era domain
+#'
+#' @param conceptSet A condition concept set
+#' @param ... optional attributes
+#'
+#' @return A Capr Query
+#' @export
+conditionEra <- function(conceptSet, ...) {
+
+  query(domain = "ConditionEra",
+        conceptSet = conceptSet,
+        ...)
+}
+
+# Serialization -----
+
+setMethod("as.list", "Query", function(x) {
+
+  # ll <- list("ConceptSets"
+  #   x@Domain = list(
+  #            'CodesetId' = x@conceptSet@id,
+  #            lapply(x@Attributes, as.list)))
+
+  # if(length(ll$CodesetId) == 0){
+  #   ll$CodesetId <- NA_character_
+  # }
+  # return(ll)
+})
+
 
