1182e86bbad6924c46102282bf87cc84bfce2153
Martin Lavallee <44976592+mdlavallee92@users.noreply.github.com>
2022-12-13 14:40:48 -0500

    turn guid to digest hash on ids

--- R/collectConceptSets_tmp.R
+++ R/collectConceptSets_tmp.R
@@ -1,14 +1,14 @@
 setMethod("collectConceptSets", "Query", function(x) {
   tmp <- x@conceptSet
   as.list(tmp)
-}
+})
 
 
 setMethod("collectConceptSets", "Count", function(x) {
   collectConceptSets(x@query)
-}
+})
 
 setMethod("collectConceptSets", "Group", function(x) {
   purrr::map(x@criteria, ~collectConceptSets(.x))
-}
+})
 
