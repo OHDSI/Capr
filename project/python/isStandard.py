6fcd12e5c957b25ab1d01cf73c1c5da701b3d959
Guus Wilmink <guus.wilmink@gmail.com>
2024-07-09 11:35:44 +0200

    index=False update for standard concept check save file

--- project/python/isStandard.py
+++ project/python/isStandard.py
@@ -7,7 +7,8 @@ concept_table = pd.read_csv('./project/data/vocabs_14-May-2024/CONCEPT.csv',
                             sep='\t',
                             dtype={'concept_id': object})
 # Clean concept_id values
-concept_table['concept_id'] = concept_table['concept_id'].apply(lambda x: x.strip().lower())
+concept_table['concept_id'] = concept_table['concept_id'].apply(
+    lambda x: x.strip().lower())
 
 # Specify the path to the tables with the source code/terms and concept_id
 tables_path = './project/data/phems_variable_list/'
@@ -26,15 +27,17 @@ for table_name in tables:
     table = pd.read_csv(
         tables_path + table_name,
         dtype={'sourceCode': object, 'concept_id': object}
-    # Drop empty rows
+        # Drop empty rows
     ).dropna(how='all')
     # Clean concept_id values
-    table['concept_id'] = table['concept_id'].apply(lambda x: x.strip().lower())
+    table['concept_id'] = table['concept_id'].apply(
+        lambda x: x.strip().lower())
 
     # Inner join the concept table with the table of source code/terms and concept_id
     joined = concept_table.merge(table, on='concept_id', how='inner')
     # Keep only relevant columns
     res = joined.filter(items=['sourceCode', 'concept_id', 'standard_concept'])
-    
+
     # Save the result
-    res.to_csv(save_path + table_name)
+    print('writing standard concept check file for ' + table_name + 'to ' + save_path + table_name)
+    res.to_csv(save_path + table_name, index=False)
