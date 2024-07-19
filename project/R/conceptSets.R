1c5af40dc4048b8515ae91441e8501134195748d
Guus Wilmink <guus.wilmink@gmail.com>
2024-07-19 15:24:51 +0200

    update conceptSets.R

--- project/R/conceptSets.R
+++ project/R/conceptSets.R
@@ -283,6 +283,67 @@ conceptSets <- list(
       42537043,  # source: CVL infections /// concept: CLABSI - central line associated bloodstream infection
       4306136  # source: Surgical injury - Chylothorax /// concept: Chylothorax
     ),
+
+    # 5.15 Vital signs
+    vitalSigns = c(
+      3027018, # source: Age-dependent HR /// concept: Heart rate
+      3024171, # source: Age-dependent RR /// concept: Respiratory rate
+      3004249, # source: SBP (Systolic Blood Pressure) /// concept: Systolic blood pressure
+      3012888, # source: DBP (Dyastolic Blood Pressure) /// concept: Diastolic blood pressure
+      4020553 # source: SpO2 /// concept: Oxygen saturation measurement
+    ),
+
+    # 5.16 HSJD clinical
+    HSJDClinical = c(
+      3025315, # source: Weight /// concept: Body weight
+      3036277, # source: Height /// concept: Body height
+      4201235, # source: Body Surface Area (BSA) - Combination of weight and height /// concept: body surface area
+      4275564 # source: Cardiac surgeries (Interventions) /// concept: Operation on heart
+    ), 
+    
+    # 5.17 HSJD Surgery
+    HSJDSurgery = c(
+      40490494, # source: STAT/EACTS score of surgery /// concept: Society of Thoracic Surgeons risk calculator
+      4301351, # source: Surgery start datetime, end datetime /// concept: Surgical procedure
+      4336464, # source: Cardiac bypass start datetime, end datetime /// concept: Coronary artery bypass graft
+      4201547, # source: Cross-clamping start datetime, end datetime /// concept: Placement of arterial cross clamp
+      37152781, # source: Deep hypothermic circulatory arrest start datetime, end datetime /// concept: Hypothermic circulatory arrest
+      4272324, # source: Antegrade Cerebral Perfusion start datetime, end datetime /// concept: Cardiopulmonary bypass operation
+      4150627 # source: Extubation event /// concept: Removal of endotracheal tube
+    ),
+
+    # 5.18 HSJD Hospitalisations 
+    HSJDHospitalisation = c(
+      44803020, # source: Admission reason /// concept: Primary reason for admission
+      42539650, # source: Vasoactive infusion start datetime, end datetime /// concept: Administration of intravenous vasoactive drug
+      4179206, # source: Postop central venous catheter in/start datetime, out/end datetime /// concept: Central venous catheter
+      37158404, # source: Invasive Mechanical Ventilation LOS /// concept: Invasive mechanical ventilation
+      4177224 # source: Non-Invasive Mechanical Ventilation LOS /// concept: Non-invasive ventilation
+    ),
+
+    # 5.19 HSJD Complications
+    HSJDcomplications = c(
+      4202832, # source: intubation /// concept: Intubation
+      4275564, # source: Cardiac surgeries (interventions) /// concept: Operation on heart
+      4044892, # source: Sternum reopening (procedure undertaken) /// concept: Procedure on sternum
+      4046868, # source: Vocal Cord Dysfunction /// concept: Vocal cord dysfunction
+      4275136, # source: Diaphragmatic Paralysis /// concept: Paralysis of diaphragm
+      253796, # source: Pneumothorax /// concept: Pneumothorax
+      44783799, # source: Tracheostomy /// concept: Exteriorization of trachea
+      44784217, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Cardiac arrhythmia
+      315643, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Tachyarrhythmia
+      4228448, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Bradyarrhythmia
+      4068155, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Atrial arrhythmia
+      4185572, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Ventricular arrhythmia
+      46234437, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: ivabradine
+      1309944, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: amiodarone
+      1354860, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: flecainide
+      4353741, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Cardioversion
+      4117045, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Atrial overdrive pacing
+      4049398, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Temporary cardiac pacemaker procedure
+      4051940, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Permanent cardiac pacemaker procedure
+      609312 # source: Listed for heart transplantation /// concept: Awaiting transplantation of heart
+      ),
     
     # 5.21 healthcare use
     healthcareUse = c(
@@ -432,67 +493,6 @@ conceptSets <- list(
       3024354, # source: PaO2 /// concept: Oxygen [Partial pressure] in Venous blood
       3012544 # source: pH /// concept: pH of Venous blood
     ),
-
-    # 
-    vitalSigns = c(
-      3027018, # source: Age-dependent HR /// concept: Heart rate
-      3024171, # source: Age-dependent RR /// concept: Respiratory rate
-      3004249, # source: SBP (Systolic Blood Pressure) /// concept: Systolic blood pressure
-      3012888, # source: DBP (Dyastolic Blood Pressure) /// concept: Diastolic blood pressure
-      4020553 # source: SpO2 /// concept: Oxygen saturation measurement
-    ),
-
-    # 5.16 HSJD clinical
-    HSJDClinical = c(
-      3025315, # source: Weight /// concept: Body weight
-      3036277, # source: Height /// concept: Body height
-      4201235, # source: Body Surface Area (BSA) - Combination of weight and height /// concept: body surface area
-      4275564 # source: Cardiac surgeries (Interventions) /// concept: Operation on heart
-    ), 
-    
-    # 5.17 HSJD Surgery
-    HSJDSurgery = c(
-      40490494, # source: STAT/EACTS score of surgery /// concept: Society of Thoracic Surgeons risk calculator
-      4301351, # source: Surgery start datetime, end datetime /// concept: Surgical procedure
-      4336464, # source: Cardiac bypass start datetime, end datetime /// concept: Coronary artery bypass graft
-      4201547, # source: Cross-clamping start datetime, end datetime /// concept: Placement of arterial cross clamp
-      37152781, # source: Deep hypothermic circulatory arrest start datetime, end datetime /// concept: Hypothermic circulatory arrest
-      4272324, # source: Antegrade Cerebral Perfusion start datetime, end datetime /// concept: Cardiopulmonary bypass operation
-      4150627 # source: Extubation event /// concept: Removal of endotracheal tube
-    ),
-
-    # 5.18 HSJD Hospitalisations 
-    HSJDHospitalisation = c(
-      44803020, # source: Admission reason /// concept: Primary reason for admission
-      42539650, # source: Vasoactive infusion start datetime, end datetime /// concept: Administration of intravenous vasoactive drug
-      4179206, # source: Postop central venous catheter in/start datetime, out/end datetime /// concept: Central venous catheter
-      37158404, # source: Invasive Mechanical Ventilation LOS /// concept: Invasive mechanical ventilation
-      4177224 # source: Non-Invasive Mechanical Ventilation LOS /// concept: Non-invasive ventilation
-    ),
-
-    # 5.19 HSJD Complications
-    HSJDcomplications = c(
-      4202832, # source: intubation /// concept: Intubation
-      4275564, # source: Cardiac surgeries (interventions) /// concept: Operation on heart
-      4044892, # source: Sternum reopening (procedure undertaken) /// concept: Procedure on sternum
-      4046868, # source: Vocal Cord Dysfunction /// concept: Vocal cord dysfunction
-      4275136, # source: Diaphragmatic Paralysis /// concept: Paralysis of diaphragm
-      253796, # source: Pneumothorax /// concept: Pneumothorax
-      44783799, # source: Tracheostomy /// concept: Exteriorization of trachea
-      44784217, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Cardiac arrhythmia
-      315643, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Tachyarrhythmia
-      4228448, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Bradyarrhythmia
-      4068155, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Atrial arrhythmia
-      4185572, # source: Arrhythmia diagnosis (tachyarrhythmia, bradyarrhythmia, atrial, ventricular etc.) /// concept: Ventricular arrhythmia
-      46234437, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: ivabradine
-      1309944, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: amiodarone
-      1354860, # source: Drugs administered; Arrhythmia therapy (drugs): IV anthyarritmics (ivabradine, amiodarone, flecainide) /// concept: flecainide
-      4353741, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Cardioversion
-      4117045, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Atrial overdrive pacing
-      4049398, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Temporary cardiac pacemaker procedure
-      4051940, # source: Arrhythmia therapy (procedures): Cardioversion, Rapid atrial pacing, temporary pacing, permanent pacing /// concept: Permanent cardiac pacemaker procedure
-      609312 # source: Listed for heart transplantation /// concept: Awaiting transplantation of heart
-      ),
 
     # 5.28 Patient characteristics 
     UC3PateintCharacteristics = c(
