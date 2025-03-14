97c114a1e69beca8047dea0d4976a64ab9b66dbd
Lavallee,Dr.,Martin (MED MA) BIP-US-R <martin.lavallee@boehringer-ingelheim.com>
2025-03-14 16:19:28 -0400

    add attribute coercion

--- R/toCapr.R
+++ R/toCapr.R
@@ -1,15 +1,18 @@
-conceptSetToCapr <- function(cs) {
+# Concept Set -----------------
+# function to coerce concept sets to capr cs
+conceptSetToCapr <- function(ccs) {
   #get concept ids from concept set expressions
-  cid <- purrr::map(purrr::map(cs, function(x) x$expression$items),
+  cid <- purrr::map(purrr::map(ccs, function(x) x$expression$items),
                     function(y) purrr::map_int(y, function(z) z$concept$CONCEPT_ID))
   #get names from concept set expressions
-  nm <- purrr::map(cs, function(x) x$name)
+  nm <- purrr::map(ccs, function(x) x$name)
   # turn into Capr concept set
   caprCs <- purrr::map2(cid, nm, ~cs(.x, name = .y))
 
   #get mapping from concept set exrpessions (i.e includeDescendants, isExcluded)
   #if there is no mapping the function will return a NULL
-  mapping <- purrr::map(purrr::map(cs, function(x) x$expression$items),
+  `%notin%` <- Negate("%in%")
+  mapping <- purrr::map(purrr::map(ccs, function(x) x$expression$items),
                         function(y) purrr::map(y, function(z) names(z)[names(z) %notin% "concept"]))
 
 
@@ -34,25 +37,148 @@ conceptSetToCapr <- function(cs) {
 
 }
 
-
-getCsKey <- function(cs, caprCs) {
-  oid <- purrr::map_int(cs, ~.x$id)
+# function to create concept set merge key
+getCsKey <- function(ccs, caprCs) {
+  #get the old id
+  oid <- purrr::map_int(ccs, ~.x$id)
+  #find new capr hash
   caprHash <- purrr::map_chr(caprCs, ~.x@id)
+  #make a merge key
   tb <- tibble::tibble(
     oid = oid,
     caprHash = caprHash
   ) |>
     dplyr::mutate(
+      #order by apperance
       idx = dplyr::row_number(), .before = 1
     )
   return(tb)
 }
+ # Attributes -------------------------
+
+conceptAttributeToCapr <- function(attribute, name, type = NULL) {
+
+  cl <- purrr::map(
+    attribute,
+    ~newConcept(
+      id = .x$CONCEPT_ID,
+      conceptName = .x$CONCEPT_NAME,
+      conceptCode = .x$CONCEPT_CODE,
+      domainId = .x$DOMAIN_ID,
+      invalidReasonCaption = .x$INVALID_REASON_CAPTION,
+      standardConceptCaption = .x$STANDARD_CONCEPT_CAPTION,
+      vocabularyId = .x$VOCABULARY_ID
+    )@Concept
+  )
+  ca <- methods::new("conceptAttribute", name = name, conceptSet = list(cl))
+  return(ca)
+}
+
+dateAdjustmentAttributeToCapr <- function(attribute, name, type = NULL) {
+  daa <- methods::new("dateAdjustmentAttribute",
+               startWith = attribute$StartWith,
+               startOffset = attribute$StartOffset,
+               endWith = attribute$EndWith,
+               endOffset = attribute$EndOffset)
+  return(daa)
+}
+
+logicAttributeToCapr <- function(attribute, name, type = NULL) {
+  la <- methods::new("logicAttribute", name = name)
+  return(la)
+}
+
+
+opAttributeToCapr <- function(attribute, name, type) {
+
+  if (type == "numeric") {
+    if (is.null(attribute$Extent)) {
+      extent <- NA_real_
+    }
+
+    opa <- methods::new("opAttributeNumeric",
+                        name = name,
+                        op = attribute$Op,
+                        value = attribute$Value,
+                        extent = extent)
+  }
+
+  if (type == "integer") {
+    if (is.null(attribute$Extent)) {
+      extent <- NA_integer_
+    } else{
+      extent <- as.integer(attribute$Extent)
+    }
+
+    opa <- methods::new("opAttributeInteger",
+                        name = name,
+                        op = attribute$Op,
+                        value = as.integer(attribute$Value),
+                        extent = extent)
+  }
+
+  if (type == "date") {
+    if (is.null(attribute$Extent)) {
+      extent <- lubridate::NA_Date_
+    } else {
+      extent <- lubridate::as_date(attribute$Extent)
+    }
+
+    opa <- methods::new("opAttributeDate",
+                        name = name,
+                        op = attribute$Op,
+                        value = lubridate::as_date(attribute$Value),
+                        extent = extent)
+  }
+
+  return(opa)
+
+}
+
+attributesToCapr <- function(x) {
+
+  #get the attribute names
+  attributeNames <- names(x)[which(names(x) != "CodesetId")]
+  # get the attribute list
+  circeAttributeList <- x[which(names(x) != "CodesetId")]
+
+  # pull the merge key
+  attributeKey <- readr::read_csv(
+    file = fs::path_package(package = "Capr", "csv/attributeKey.csv"),
+    show_col_types = FALSE
+  ) |>
+    dplyr::filter(
+      attributeName %in% attributeNames
+    )
+
+  caprAttributeList <- vector('list', length = length(circeAttributeList))
+  for (i in seq_along(caprAttributeList)) {
+    caprAttrFn <- attributeKey$functionCall[i]
+    name <- attributeKey$attributeName[i]
+    if (!is.na(attributeKey$attributeType[i])) {
+      type <- attributeKey$attributeType[i]
+    } else {
+      type <- NULL
+    }
+    caprAttrCall <- rlang::call2(
+      caprAttrFn,
+      attribute = circeAttributeList[[i]],
+      name = name,
+      type = type
+    )
+    caprAttributeList[[i]] <- eval(caprAttrCall)
+  }
+
+  return(caprAttributeList)
+}
+
+# Query --------------------
 
 # function to turn a circe query into capr
 queryToCapr <- function(x, caprCs, csTb) {
 
   # get the capr domain call
-  domain <- names(x) |> SqlRender::snakeCaseToCamelCase()
+  domain <- names(x) |> snakecase::to_lower_camel_case()
 
   # get the caprCs index of the concept set
   idx <- csTb |>
@@ -64,11 +190,13 @@ queryToCapr <- function(x, caprCs, csTb) {
   # pull out the query cs
   queryCs <- caprCs[[idx]]
 
+  attributeList <- attributesToCapr(x[[1]])
+
   # make a query call
   queryCall <- rlang::call2(
     domain,
-    queryCs
-    #TODO add attributes to query call
+    queryCs,
+    !!!attributeList
   )
 
   res <- eval(queryCall)
@@ -77,6 +205,7 @@ queryToCapr <- function(x, caprCs, csTb) {
 
 }
 
+# Criteria ----------------------------
 #function to turn a circe count into a capr criteria
 criteriaToCapr <- function(x, caprCs, csTb) {
   # prep query
@@ -127,18 +256,47 @@ criteriaToCapr <- function(x, caprCs, csTb) {
   return(res)
 }
 
+# Group -----------------------------
 #function to turn circe group into capr group
 groupToCapr <- function(x, caprCs, csTb) {
+  # get the fn call
+  tt <- as.character(x$Type)
+  caprCall <- switch(
+    tt,
+    'ALL' = "withAll",
+    'ANY' = "withAny",
+    'AT_LEAST' = "withAtLeast",
+    'AT_MOST' = "withAtMost"
+  )
 
+  #get the criteriaList first
+  criteriaList <- purrr::map(x$CriteriaList, ~criteriaToCapr(.x, caprCs = caprCs, csTb = csTb))
+
+  if (any(caprCall %in% c("withAtLeast", "withAtMost"))) {
+    amt <- x$Count
+    groupCall <- rlang::call2(
+      caprCall,
+      x = amt,
+      !!!groupList
+    )
+  } else {
+    groupCall <- rlang::call2(
+      caprCall,
+      !!!criteriaList
+    )
+  }
+
+  res <- eval(groupCall)
 }
 
+# Primary Criteria -------------------
 # function to turn a circe primary criteria into capr
 primaryCriteriaToCapr <- function(cd) {
 
   # get concept sets for arrangement
-  cs <- cd$ConceptSets
-  caprCs <- conceptSetToCapr(cs)
-  csTb <- getCsKey(cs, caprCs)
+  ccs <- cd$ConceptSets
+  caprCs <- conceptSetToCapr(ccs)
+  csTb <- getCsKey(ccs, caprCs)
 
   pc <- cd$PrimaryCriteria
 
@@ -161,11 +319,13 @@ primaryCriteriaToCapr <- function(cd) {
   res@observationWindow <- ctsObs
   res@primaryCriteriaLimit <- pcL
   res@qualifiedLimit <- cd$QualifiedLimit$Type
+  #TODO additional criteria with groups
 
   return(res)
 
 }
 
+# Inclusion Rules -------------------------------------
 # function to covert inclusion rules to Capr
 inclusionRulesToCapr <- function(cd) {
   # get concept sets for arrangement
@@ -177,7 +337,7 @@ inclusionRulesToCapr <- function(cd) {
 }
 
 
-
+# Exit ---------------------------
 # function to covert end strategy and censor to Capr
 exitToCapr <- function(cd) {
   # get concept sets for arrangement
@@ -188,6 +348,7 @@ exitToCapr <- function(cd) {
   censor <- cd$CensoringCriteria
 }
 
+# Era --------------------
 # function to convert collapse to Capr
 eraToCapr <- function(cd) {
 
@@ -200,17 +361,37 @@ eraToCapr <- function(cd) {
   return(res)
 }
 
-
+# UI -----------------------------
 #' Read a circe json definition and convert it to a Capr cohort definition
 #' @export
 #' @param path character string that must be a valid path to read json from
 readInCirce <- function(path) {
   checkmate::assertFileExists(path)
+
+  # import json file into R
   circeObj <- jsonlite::fromJSON(path)
   checkmate::assertList(circeObj)
+
+  # coerce circe primary criteria, qualified and additional criteria into Capr entry
   entry <- primaryCriteriaToCapr(circeObj)
 
-  Capr::cohort(entry = entry)
+  # coerce circe inclusion rules and expression limit into Capr attrition
+  attrition <- attritionToCapr(circeObj)
+
+  # coerce circe censoring criteria and end strategy into Capr exit
+  exit <- exitToCapr(circeObj)
+
+  # coerce circe collapse settings and censor window into Capr era
+  era <- eraToCapr(circeObj)
+
+  # combine sub components into a Capr cohort
+  caprDef <- Capr::cohort(
+    entry = entry,
+    attrition = attrition,
+    exit = exit,
+    era = era
+  )
+
   return(caprDef)
 }
 
